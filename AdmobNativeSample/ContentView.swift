c12e548183c797277232f2fe12eab0fa5eda77b9
Yukari Sakurai <neko3genki@gmail.com>
2021-05-08 02:39:43 +0900

    Add comment

--- AdmobNativeSample/ContentView.swift
+++ AdmobNativeSample/ContentView.swift
@@ -11,6 +11,8 @@ struct ContentView: View {
 
   var body: some View {
     GADNativeViewControllerWrapper()
+      // The placeholder for GADUnifiedNativeAdView in the original sample code specified a constraint 'height 300'
+      //https://github.com/googleads/googleads-mobile-ios-examples/blob/10cc05c23af491492edc7ac0ac9a10a93651a6f7/Swift/admob/NativeAdvancedExample/NativeAdvancedExample/Base.lproj/Main.storyboard#L40
       .frame(height: 300)
       .padding(10)
   }
